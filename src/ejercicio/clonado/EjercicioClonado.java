966a2bd8c956263539d5598b470825d14d7fca96
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2017-10-20 08:49:44 +0200

    1 commit David.

---
+++ src/ejercicio/clonado/EjercicioClonado.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.clonado;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class EjercicioClonado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO");
+        System.out.println("CLONADO");
+    }
+    
+}
